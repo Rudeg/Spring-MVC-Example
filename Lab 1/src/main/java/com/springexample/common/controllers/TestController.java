635ddb2a9865d53e45ec9063e9f4c75451759ea8
unknown <rudenko.sergey92@gmail.com>
2013-09-10 08:55:53 +0300

    small refactoring

--- Lab 1/src/main/java/com/springexample/common/controllers/TestController.java
+++ Lab 1/src/main/java/com/springexample/common/controllers/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TestController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
+    public String testMethod(ModelMap model) {
         model.addAttribute("message", "Test message is here");
         return "index";
     }
