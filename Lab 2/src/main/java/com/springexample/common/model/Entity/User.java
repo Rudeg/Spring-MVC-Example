550153ac7cdad2f1421eef9cf7e623d6ac2e35cf
sergey rudenko <rudenko.sergey92@gmail.com>
2013-10-06 01:30:26 +0300

    add login from db

--- Lab 2/src/main/java/com/springexample/common/model/Entity/User.java
+++ Lab 2/src/main/java/com/springexample/common/model/Entity/User.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @FieldMatch.List({
         @FieldMatch(first = "password", second = "repeatPassword", message = "The password fields must match"),
@@ -52,9 +53,14 @@ public class User extends BaseEntity<Long> {
     private Boolean receiveNewsLetter;
 
     @Column(nullable = false)
-    //dropdown
     @NotBlank
     private String country;
+
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "ex_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> userRoles;
+
 
     // getters and setters...
     public String getUsername() {
@@ -127,5 +133,13 @@ public class User extends BaseEntity<Long> {
 
     public void setReceiveNewsLetter(Boolean receiveNewsLetter) {
         this.receiveNewsLetter = receiveNewsLetter;
+    }
+
+    public Set<Role> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<Role> userRoles) {
+        this.userRoles = userRoles;
     }
 }
